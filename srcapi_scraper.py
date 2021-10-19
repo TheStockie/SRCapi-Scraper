b5311ba2f0a21bcefcefbca4a767a91a1ee9a0d0
Jaime Stockwell Mendoza <StockWellGamez@gmail.com>
2021-10-19 21:55:21 +0200

    Created function per game

--- srcapi_scraper.py
+++ srcapi_scraper.py
@@ -30,7 +30,7 @@ def run_scraper(game, game_dict):
 
 def run_ini_simple(r, current_run):
   if len(r.values.values()) > 0:
-    subcategory_parse(list(r.values.values())[0], current_run)
+    subcategory_parse(list(r.values.values())[0], current_run)  
   current_run["player"] = r.players
   current_run["times"] = r.times
   current_run["videos"] = r.videos
@@ -69,47 +69,60 @@ subcategory_dict_ini()
 ### --------------------------------------------------------- ###
 # ----- Example Code Implementation: replace all "game_name" instances with whatever game you're going to extract -----
 # ----- Best practice to write the exact name in "game name" at the end -----
-# game_name = api.search(srcomapi.datatypes.Game, {"name": "game name"})[0] 
-# game_name_runs = {}
-# run_scraper(game_name, game_name_runs)
-# game_name_csv = open("game_name.csv", "w", encoding='utf-8')
-# game_name_writer = csv.writer(game_name_csv)
-# ----- Simple flag is for the complexity of the data. True for Simple, False for complex -----
-# run_parse(game_name_runs.keys(), game_name_writer, game_name_runs, simple_flag)
-# game_name_csv.close()
+# def game_name():
+#   game_name = api.search(srcomapi.datatypes.Game, {"name": "game name"})[0] 
+#   game_name_runs = {}
+#   run_scraper(game_name, game_name_runs)
+#   game_name_csv = open("game_name.csv", "w", encoding='utf-8')
+#   game_name_writer = csv.writer(game_name_csv)
+#   ----- Simple flag is for the complexity of the data. True for Simple, False for complex -----
+#   run_parse(game_name_runs.keys(), game_name_writer, game_name_runs, simple_flag)
+#   game_name_csv.close()
 
 # ICO
-ico = api.search(srcomapi.datatypes.Game, {"name": "ico"})[0]
-ico_runs = {}
-run_scraper(ico, ico_runs)
-ico_csv = open("ico.csv", "w", encoding='utf-8')
-ico_writer = csv.writer(ico_csv)
-run_parse(ico_runs.keys(), ico_writer, ico_runs, True)
-ico_csv.close()
+def ico():
+  ico = api.search(srcomapi.datatypes.Game, {"name": "ico"})[0]
+  ico_runs = {}
+  run_scraper(ico, ico_runs)
+  ico_csv = open("ico.csv", "w", encoding='utf-8')
+  ico_writer = csv.writer(ico_csv)
+  run_parse(ico_runs.keys(), ico_writer, ico_runs, True)
+  ico_csv.close()
 
 # SotC
-sotc = api.search(srcomapi.datatypes.Game, {"name": "shadow of the colossus"})[0]
-sotc_runs = {}
-run_scraper(sotc, sotc_runs)
-sotc_csv = open("sotc.csv", "w", encoding='utf-8')
-sotc_writer = csv.writer(sotc_csv)
-run_parse(sotc_runs.keys(), sotc_writer, sotc_runs, True)
-sotc_csv.close()
+def sotc():
+  sotc = api.search(srcomapi.datatypes.Game, {"name": "shadow of the colossus"})[0]
+  sotc_runs = {}
+  run_scraper(sotc, sotc_runs)
+  sotc_csv = open("sotc.csv", "w", encoding='utf-8')
+  sotc_writer = csv.writer(sotc_csv)
+  run_parse(sotc_runs.keys(), sotc_writer, sotc_runs, True)
+  sotc_csv.close()
 
 # SotC 2018
-sotc_2018 = api.search(srcomapi.datatypes.Game, {"name": "shadow of the colossus (2018)"})[0]
-sotc_2018_runs = {}
-run_scraper(sotc_2018, sotc_2018_runs)
-sotc_2018_csv = open("sotc_2018.csv", "w", encoding='utf-8')
-sotc_2018_writer = csv.writer(sotc_2018_csv)
-run_parse(sotc_2018_runs.keys(), sotc_2018_writer, sotc_2018_runs, True)
-sotc_2018_csv.close()
+def sotc_2018():
+  sotc_2018 = api.search(srcomapi.datatypes.Game, {"name": "shadow of the colossus (2018)"})[0]
+  sotc_2018_runs = {}
+  run_scraper(sotc_2018, sotc_2018_runs)
+  sotc_2018_csv = open("sotc_2018.csv", "w", encoding='utf-8')
+  sotc_2018_writer = csv.writer(sotc_2018_csv)
+  run_parse(sotc_2018_runs.keys(), sotc_2018_writer, sotc_2018_runs, True)
+  sotc_2018_csv.close()
 
 # TLG
-tlg = api.search(srcomapi.datatypes.Game, {"name": "the last guardian"})[0]
-tlg_runs = {}
-run_scraper(tlg, tlg_runs)
-tlg_csv = open("tlg.csv", "w", encoding="utf-8")
-tlg_writer = csv.writer(tlg_csv)
-run_parse(tlg_runs.keys(), tlg_writer, tlg_runs, True)
-tlg_csv.close()
+def tlg():
+  tlg = api.search(srcomapi.datatypes.Game, {"name": "the last guardian"})[0]
+  tlg_runs = {}
+  run_scraper(tlg, tlg_runs)
+  tlg_csv = open("tlg.csv", "w", encoding="utf-8")
+  tlg_writer = csv.writer(tlg_csv)
+  run_parse(tlg_runs.keys(), tlg_writer, tlg_runs, True)
+  tlg_csv.close()
+
+### --------------------------------------------------------- ###
+# ----- Execution of code -----
+# game_name()
+ico()
+sotc()
+sotc_2018()
+tlg()
